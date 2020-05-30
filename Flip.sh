526d73a385f6b46801c0fd7b3ccb05bd00eaa06e
suyog-6996 <gabhane.suyog@gmail.com>
2020-05-30 09:52:40 +0530

    ADD:Counting H and T

--- Flip.sh
+++ Flip.sh
@@ -1,10 +1,22 @@
-echo "Welcome to Flip Coin Simulator"
-flip=0;
-random=$(( RANDOM % 2 ));
+echo "Welcome to Flip Coin Simulation";
 
-if [ $flip -eq $random ]
-then
-	echo "Head";
-else
-	echo "Tail";
-fi
+flip=0;
+isHeadWon=0;
+isTailWon=0;
+
+for (( count=0; count<=20; count++ ))
+do
+	random=$(( RANDOM % 2 ));
+
+	if [ $flip -eq $random ]
+	then
+		echo "Head";
+		isHeadWon=$(( isHeadWon + 1 ));
+	else
+		echo "Tail";
+		isTailWon=$(( isTailWon + 1 ));
+	fi
+done
+
+echo "Total Number Head Won:" $isHeadWon;
+echo "Total Number Tail Won:" $isTailWon;
