6eb3a4f192afa7dcc3ce79f07d9ac3c1ef6ee150
suyog-6996 <gabhane.suyog@gmail.com>
2020-05-30 10:15:58 +0530

    ADD:Winning Diff loop

--- Flip.sh
+++ Flip.sh
@@ -1,22 +1,32 @@
-echo "Welcome to Flip Coin Simulation";
+echo "Welcome to Flip Coin Simulator";
 
 flip=0;
 isHeadWon=0;
 isTailWon=0;
 
-for (( count=0; count<=20; count++ ))
+while [[ $isHeadWon -lt 21 && $isTailWon -lt 21 ]]
 do
 	random=$(( RANDOM % 2 ));
 
 	if [ $flip -eq $random ]
 	then
-		echo "Head";
 		isHeadWon=$(( isHeadWon + 1 ));
 	else
-		echo "Tail";
 		isTailWon=$(( isTailWon + 1 ));
 	fi
 done
 
 echo "Total Number Head Won:" $isHeadWon;
 echo "Total Number Tail Won:" $isTailWon;
+
+if [[ $isHeadWon -gt $isTailWon ]]
+then
+	difference=$(($isHeadWon - $isTailWon ));
+	echo "Head Wins times more than Tail by" $difference;
+elif [[ $isHeadwon -eq $isTailWon ]]
+then
+	echo "Tie between Head and Tail";
+else
+	difference=$(($isTailWon - $isHeadWon ));
+	echo "Tail Wins times more than Head by" $difference;
+fi
